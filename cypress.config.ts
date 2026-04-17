9925687bec7f500906222f40ba52f927c8627061
Sergio Suárez <sergiosgil@gmail.com>
2026-04-18 00:13:09 +0200

    fix: preview port config

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'cypress';
 export default defineConfig({
   allowCypressEnv: false,
   e2e: {
-    baseUrl: 'http://localhost:5173',
+    baseUrl: process.env.E2E_BASE_URL || 'http://localhost:5173',
     setupNodeEvents(on, config) {},
   },
 });
--- playwright.config.ts
+++ playwright.config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   reporter: 'list',
   use: {
-    baseURL: 'http://localhost:5173',
+    baseURL: process.env.E2E_BASE_URL || 'http://localhost:5173',
     trace: 'on-first-retry',
   },
   projects: [
