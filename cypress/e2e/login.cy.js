dd2952d64ed5f410f11b5f5020d6761858aa55f5
Sergio Suárez <sergiosgil@gmail.com>
2026-04-17 23:57:50 +0200

    chore: force trigger

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -2,6 +2,7 @@ describe('Login Scene', () => {
   it('should render login form', () => {
     // Arrange & Act
     cy.visit('/login');
+
     // Assert
     cy.get('input[name="user"]').should('exist');
     cy.get('input[name="password"]').should('exist');
@@ -11,19 +12,23 @@ describe('Login Scene', () => {
   it('should show validation error if fields are empty', () => {
     // Arrange
     cy.visit('/login');
+
     // Act
     cy.get('button[type="submit"]').click();
+
     // Assert
     cy.contains('Debe informar el campo').should('exist');
   });
 
   it('should show error with invalid credentials', () => {
     // Arrange
+
     cy.visit('/login');
     // Act
     cy.get('input[name="user"]').type('wrong');
     cy.get('input[name="password"]').type('wrong');
     cy.get('button[type="submit"]').click();
+
     // Assert
     cy.contains('Usuario y/o password no válidos').should('exist');
   });
@@ -31,10 +36,12 @@ describe('Login Scene', () => {
   it('should login with valid credentials and navigate', () => {
     // Arrange
     cy.visit('/login');
+
     // Act
     cy.get('input[name="user"]').type('admin');
     cy.get('input[name="password"]').type('test');
     cy.get('button[type="submit"]').click();
+
     // Assert
     cy.url().should('include', '/submodule-list');
   });
